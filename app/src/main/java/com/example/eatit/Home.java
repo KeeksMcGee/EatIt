92067c81fdbba030dd8506112ffd456a76e0cd47
KeeksMcGee <51935561+KeeksMcGee@users.noreply.github.com>
2019-11-21 14:08:54 -0500

    Update Home.java

--- app/src/main/java/com/example/eatit/Home.java
+++ app/src/main/java/com/example/eatit/Home.java
@@ -11,8 +11,12 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -34,7 +38,7 @@ import android.view.Menu;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private AppBarConfiguration mAppBarConfiguration;
 
@@ -68,26 +72,21 @@ public class Home extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               Intent cartIntent = new Intent(Home.this,Cart.class);
-               startActivity(cartIntent);
+                Intent cartIntent = new Intent(Home.this,Cart.class);
+                startActivity(cartIntent);
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-            this, drawer, toolbar, "Open navigaton drawer", "Close navigaton drawer");
-        drawer.setDrawerListener(toggle);
-        roggle.syncState();
-        
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
         NavigationView navigationView = findViewById(R.id.nav_view);
-             navigation.view.setNavigationItemSelectedListener(this);
+        navigationView.setNavigationItemSelectedListener(this);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_cart, R.id.nav_orders,
-                R.id.nav_log_out)
-                .setDrawerLayout(drawer)
-                .build();
-   
+
 
         //Set Name for user
         View headerView = navigationView.getHeaderView(0);
@@ -102,13 +101,13 @@ public class Home extends AppCompatActivity {
 
         loadMenu();
     }
-    
+
     private void loadMenu() {
 
-         adapter = new FirebaseRecyclerAdapter<Category, MenuViewHolder>(Category.class,R.layout.menu_item,MenuViewHolder.class,category) {
+        adapter = new FirebaseRecyclerAdapter<Category, MenuViewHolder>(Category.class,R.layout.menu_item,MenuViewHolder.class,category) {
             @Override
             protected void populateViewHolder(MenuViewHolder menuViewHolder, Category category, int i) {
-            menuViewHolder.txtMenuName.setText(category.getName());
+                menuViewHolder.txtMenuName.setText(category.getName());
                 Picasso.with(getBaseContext()).load(category.getImage())
                         .into(menuViewHolder.imageView);
                 final Category clickItem = category;
@@ -126,55 +125,49 @@ public class Home extends AppCompatActivity {
         };
         recycler_menu.setAdapter(adapter);
     }
-    
+
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayou) findViewByID(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closerDrawer(GravityCompat.START);
+            drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.OnBackPressed();
-               }
+            super.onBackPressed();
+        }
     }
-    
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        //Handle navigation view item clicks here.
+        int id = item.getItemId();
 
-    @Override
-    public boolean onOptionsSelected(MenuItem item) {
-        //Handle action bar item  clicks here. The action bar will 
-        //automatically handle clicks on the Home/Up button, so long 
-        //as you specify a parent acivity in the AndroidManifest.xml
-        int id - item.getItemId();
-        
-        if (id == R.id.acton_settings) {
+        if (id == R.id.nav_home) {
             return true;
-        }
-        return super.onOptionsItemSelected(MenuItem item);
-    }
-    
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemsSelected(MenuItem item) {
-    //Handle navigaton view item clicks here.    
-    int id = item.getItemID();
-        
-        if (id == R.id.nav_menu) {
-            
+
         } else if (id == R.id.nav_cart) {
-            
+            Intent cartIntent = new Intent(Home.this, Cart.class);
+            startActivity(cartIntent);
+
         } else if (id == R.id.nav_orders) {
-            
-        } else if (id == R.id.nav_signout) {
-            
+            Intent orderIntent = new Intent(Home.this, OrderStatus.class);
+            startActivity(orderIntent);
+
+        } else if (id == R.id.nav_log_out) {
+            Intent signIn = new Intent(Home.this, SignIn.class);
+            signIn.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(signIn);
+
         }
-        
-        DrawerLayout drawer = (DrawerLayout) findViewbyId(R.id.drawer_layout);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
